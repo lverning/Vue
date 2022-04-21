cb2aed4ed2c260d17f5cfc98f2bb6e1c61671cad
Lverning <74942379+lverning@users.noreply.github.com>
2022-04-21 22:34:33 +0800

    Add files via upload

---
+++ AddCommodity.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-upload
+      class="upload-demo"
+      :action="objdata.host"
+      :before-upload="ossPolicy"
+      :file-list="fileList"
+      list-type="picture">
+    <el-button size="small" type="primary">点击上传</el-button>
+    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+  </el-upload>
+</template>
+
+<script>
+export default {
+  name: "AddCommodity",
+  data(){
+    return{
+      objdata:{
+        OSSAccessKeyId:'',
+        policy:'',
+        Signatrue:'',
+        key:'',
+        host:'',
+        dir:''
+      }
+    }
+  },
+  methods: {
+    // ossPolicy(file){
+    //   return new Promise((resolve,reject)=>{
+    //
+    //       });
+    // },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePreview(file) {
+      console.log(file);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
